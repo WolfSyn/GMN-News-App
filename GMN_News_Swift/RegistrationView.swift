44b048d4ef58be0e260bf0045d284e57f266f131
WolfSyn <115028079+WolfSyn@users.noreply.github.com>
2024-12-08 13:21:36 -0600

    Update RegistrationView.swift

--- GMN_News_Swift/RegistrationView.swift
+++ GMN_News_Swift/RegistrationView.swift
@@ -8,8 +8,108 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @EnvironmentObject var viewModel: ViewModel
+    @Environment(\.dismiss) var dismiss
+    @State private var emailText = ""
+    @State private var passwordText = ""
+    @State private var confirmPasswordText = ""
+
+    @State private var isValidEmail = true
+    @State private var isValidPassword = true
+    @State private var isConfirmPasswordValid = true
+
+    @State private var showSheet = false
+    
+    
+    var canProceed: Bool {
+        Validator.validateEmail(emailText) &&
+            Validator.validatePassword(passwordText)
+    }
+    
+    @FocusState private var focusedField: FocusedField?
+    
     var body: some View {
-        Text("RegistrationView")
+        NavigationStack {
+            ZStack {
+                if viewModel.isLoading{
+                    ProgressView()
+                }
+                VStack {
+                    Text("Create Account")
+                        .font(.system(size: 30, weight: .bold))
+                        .foregroundColor(Color("PrimaryBlue"))
+                        .padding(.bottom)
+                        .padding(.top, 48)
+                    Text("Create an account so you can explore all on what's happening in the world of Gaming")
+                        .font(.system(size: 16, weight: .medium))
+                        .foregroundColor(.black)
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 40)
+                    
+                    EmailTextField(emailText: $emailText, isValidEmail: $isValidEmail)
+                    
+                    PasswordTextField(passwordText: $passwordText, isValidPassword: $isConfirmPasswordValid, validatePassword: Validator.validatePassword, errorText: "Your password is not valid", placeholder: "Password")
+                    
+                    PasswordTextField(passwordText: $confirmPasswordText, isValidPassword: $isValidPassword, validatePassword: validateConfirm, errorText: "Your password is not matching", placeholder: "Confirm Password")
+                    
+                        .padding(.top)
+                    Spacer()
+                    
+                    
+                    Button {
+                        Task {
+                            try? await viewModel.createUser(email: emailText, password: passwordText)
+                        }
+                    } label:{
+                        Text("Sign Up")
+                            .font(.system(size: 18, weight: .semibold))
+                            .foregroundColor(.white)
+                    }
+                    .padding(.vertical)
+                    .frame(maxWidth: .infinity)
+                    .background(Color("PrimaryBlue"))
+                    .cornerRadius(12)
+                    .padding(.horizontal)
+                    .opacity(canProceed ? 1.0 : 0.5)
+                    .disabled(!canProceed)
+                    
+                    
+                    Button {
+                        showSheet.toggle()
+                    } label:{
+                        Text("Already have an account")
+                            .font(.system(size: 18, weight: .semibold))
+                            .foregroundColor(Color("Gray"))
+                    }
+                    .padding(.vertical)
+                    .frame(maxWidth: .infinity)
+                    .cornerRadius(12)
+                    .padding([.horizontal, .vertical])
+                    
+                    BottonView(googleAction: {}, facebookAction: {}, appleAction: {})
+                    Spacer()
+                }
+                .opacity(viewModel.isLoading ? 0.5 : 1.0)
+            }
+        }
+        .alert(viewModel.hasError ? "Error" : "Success", isPresented: $viewModel.showAlert) {
+            if viewModel.hasError {
+                Button("Try Again") {}
+            } else {
+                Button("Ok") {
+                    dismiss()
+                }
+            }
+        } message: {
+            Text(viewModel.alertMessage)
+        }
+        .sheet(isPresented: $showSheet) {
+            LoginView()
+        }
+    }
+    
+    func validateConfirm(_ password: String) -> Bool {
+        passwordText == password
     }
 }
 
